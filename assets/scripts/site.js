9bbd293c9a16722e9cce2f247dffc11faeec0682
Chris <chris.d.fournier@gmail.com>
2021-02-08 10:16:10 -0500

    Home page structure and content mostly complete
    Still need to add social links to footer. Much more style and detail required.

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -6,3 +6,31 @@ function doStuff (callback) {
 doStuff(function () {
   document.body.className = 'visible';
 });
+
+var myElement = document.querySelector("header");
+var headroom  = new Headroom(myElement);
+headroom.init(); 
+
+$("button.menu-toggle svg.menu-toggle-open").click(function() {
+  $("body").addClass("open");
+  $("header").addClass("open");
+  $("button.menu-toggle").attr("aria-expanded",true);
+  $("nav ul").attr("aria-hidden",false);
+});
+$("button.menu-toggle svg.menu-toggle-close").click(function() {
+  $("body").removeClass("open");
+  $("header").removeClass("open");
+  $("button.menu-toggle").attr("aria-expanded",false);
+  $("nav ul").attr("aria-hidden",true);
+});
+$(".close").click(function() {
+  $("body").removeClass("open");
+  $("header").removeClass("open");
+  $("button.menu-toggle").attr("aria-expanded",false);
+  $("nav ul").attr("aria-hidden",true);
+});
+window.onload = function() {
+  if($(window).width()>768) {
+    $("nav ul").attr("aria-hidden",false);
+  }
+};
