1eacb375989c2c40036b7d28b0063314aeeec857
Chris <chris.d.fournier@gmail.com>
2021-04-11 09:50:18 -0400

    SCSS empty setup

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,36 +1,59 @@
-function doStuff (callback) {
-  // do all app scripts here...
+// CHANGE CSS WHEN PAGE LOADS
+function loaded (callback) {
   callback();
-}
-
-doStuff(function () {
+};
+loaded(function () {
   document.body.className = 'visible';
 });
 
-var myElement = document.querySelector("header");
+
+// HEADROOM
+var myElement = document.querySelector('header');
 var headroom  = new Headroom(myElement);
 headroom.init(); 
 
-$("button.menu-toggle svg.menu-toggle-open").click(function() {
-  $("body").addClass("open");
-  $("header").addClass("open");
-  $("button.menu-toggle").attr("aria-expanded",true);
-  $("nav ul").attr("aria-hidden",false);
+
+// ANIMATE OBJECTS AS THEY COME ON SCREEN
+const addVisible = document.querySelectorAll('.animate');
+const io_options = {
+  root: null,
+  rootMargin: '0px 0px 0px 0px',
+  threshold: 0
+};
+const io_observer = new IntersectionObserver(io_callback, io_options);
+
+addVisible.forEach(element => {
+    io_observer.observe(element);
 });
-$("button.menu-toggle svg.menu-toggle-close").click(function() {
-  $("body").removeClass("open");
-  $("header").removeClass("open");
-  $("button.menu-toggle").attr("aria-expanded",false);
-  $("nav ul").attr("aria-hidden",true);
+
+function io_callback (entries, observer) {
+    entries.forEach(entry => {
+    entry.target.classList.toggle('show', entry.isIntersecting);
+  });
+};
+
+
+// MOBILE NAVIGATION
+$('button.menu-toggle svg.menu-toggle-open').click(function() {
+  $('body').addClass('open');
+  $('header').addClass('open');
+  $('button.menu-toggle').attr('aria-expanded',true);
+  $('nav ul').attr('aria-hidden',false);
 });
-$(".close").click(function() {
-  $("body").removeClass("open");
-  $("header").removeClass("open");
-  $("button.menu-toggle").attr("aria-expanded",false);
-  $("nav ul").attr("aria-hidden",true);
+$('button.menu-toggle svg.menu-toggle-close').click(function() {
+  $('body').removeClass('open');
+  $('header').removeClass('open');
+  $('button.menu-toggle').attr('aria-expanded',false);
+  $('nav ul').attr('aria-hidden',true);
+});
+$('.close').click(function() {
+  $('body').removeClass('open');
+  $('header').removeClass('open');
+  $('button.menu-toggle').attr('aria-expanded',false);
+  $('nav ul').attr('aria-hidden',true);
 });
 window.onload = function() {
   if($(window).width()>768) {
-    $("nav ul").attr("aria-hidden",false);
+    $('nav ul').attr('aria-hidden',false);
   }
 };
